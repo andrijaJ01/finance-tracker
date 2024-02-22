770ddfee3fa542afe7751577616e7411a3cb0af4
Andrija Jovanovic <andrijajovanovic001@gmail.com>
2024-02-22 17:16:00 +0100

    converting to react-bootstrap,not finished yet

--- react/finance-tracker/src/App.js
+++ react/finance-tracker/src/App.js
@@ -1,96 +1,91 @@
-import React , {useState, useEffect} from "react";
-import api from './api'
-import Button from "react-bootstrap/Button"
-import Form from "react-bootstrap/Form"
+import React, { useState, useEffect } from "react";
+import api from './api';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const App = () => {
-	const [trans,setTrans] = useState([]);
-	const [formData,setFormData] = useState({
-		amt:'',
-		cat:'',
-		desc:'',
-		isIncome:false,
-		date:''
-	});
-	const fetchTrans = async () => {
-		const response = await api.get('/transaction');
-		setTrans(response.data);
-	};
-	useEffect(()=>{
-		fetchTrans();
-	},[]);
-	const handleInputChange = (event) => {
-		const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
-		setFormData({
-			...formData,
-			[event.target.name]:value,
-		});
-	};
+    const [trans, setTrans] = useState([]);
+    const [formData, setFormData] = useState({
+        amt: '',
+        cat: '',
+        desc: '',
+        isIncome: false,
+        date: ''
+    });
 
-	const handleFormSubmit = async (event) =>{
-		event.preventDefault();
-		await api.post('/tran',formData);
-		fetchTrans();
-		setFormData({
-		amt:'',
-		cat:'',
-		desc:'',
-		isIncome:false,
-		date:''
-		});
-	};
+    const fetchTrans = async () => {
+        const response = await api.get('/transaction');
+        setTrans(response.data);
+    };
 
-	return (
-		<section>
-			<nav className="navbar navbar-dark bg-primary">
-				<div className="container-fluid">
-					<a className="navbar-brand text-dark" href="/">
-						Personal Finance Tracker
-					</a>
-				</div>
-			</nav>
+    useEffect(() => {
+        fetchTrans();
+    }, []);
 
-		<Container>
-			<Form onSubmit={handleFormSubmit}>
-				<div className="mb-3 mt-3">
-					<label htmlFor="amount" className="form-label">Amount</label>
-					<input type="text" className="form-control" id="amt" name="amt" onChange={handleInputChange} value={formData.amt} />
-				</div>
+    const handleInputChange = (event) => {
+        const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+        setFormData({
+            ...formData,
+            [event.target.name]: value,
+        });
+    };
 
-				<div className="mb-3">
-					<label htmlFor="category" className="form-label">Category</label>
-					<input type="text" className="form-control" id="cat" name="cat" onChange={handleInputChange} value={formData.cat} />
-				</div>
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        await api.post('/tran', formData);
+        fetchTrans();
+        setFormData({
+            amt: '',
+            cat: '',
+            desc: '',
+            isIncome: false,
+            date: ''
+        });
+    };
 
-				<div className="mb-3">
-					<label htmlFor="description" className="form-label">Description</label>
-					<input type="text" className="form-control" id="desc" name="desc" onChange={handleInputChange} value={formData.desc} />
-				</div>
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-dark" href="/">
+                        Personal Finance Tracker
+                    </a>
+                </div>
+            </nav>
 
+            <Container>
+	    	<Form onSubmit={handleFormSubmit}>
+	    <Row>
+                    <Form.Group className="mt-3 mb-3" controlId="amt">
+                      <Col>  <Form.Label>Enter money amount</Form.Label></Col>
+                     <Col>   <Form.Control type="text" placeholder="Enter amount" /> </Col>
+                    </Form.Group>
+	    </Row>
+                    <div className="mb-3">
+                        <label htmlFor="category" className="form-label">Category</label>
+                        <input type="text" className="form-control" id="cat" name="cat" onChange={handleInputChange} value={formData.cat} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Description</label>
+                        <input type="text" className="form-control" id="desc" name="desc" onChange={handleInputChange} value={formData.desc} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="is_income" className="form-label">Is income</label>
+                        <input type="checkbox" id="is_income" name="is_income" onChange={handleInputChange} value={formData.isIncome} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="date" className="form-label">Date</label>
+                        <input type="text" className="form-control" id="date" name="date" onChange={handleInputChange} value={formData.date} />
+                    </div>
+                    <Button type="submit" className="btn btn-primary">Submit Form</Button>
+                </Form>
+        </Container>
+        </div>
+    );
+};
 
-				<div className="mb-3">
-					<label htmlFor="is_income" className="form-label">Is income</label>
-					<input type="checkbox"  id="is_income" name="is_income" onChange={handleInputChange} value={formData.isIncome} />
-				</div>
-
-
-				<div className="mb-3">
-					<label htmlFor="date" className="form-label">Date</label>
-					<input type="text" className="form-control" id="date" name="date" onChange={handleInputChange} value={formData.date} />
-				</div>
-
-				<button type="submit" className="btn btn-primary">
-					Submit Form
-				</button>
-			</Form>
-		</Container>
-		
-		
-
-		</section>
-	)
-}
 export default App;
+
